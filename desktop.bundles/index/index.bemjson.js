3a97af5903666f585fe52f033b57205d2e8aa6a7
Varvara Stepanova <toivonen@toivonen.ru>
2013-11-19 21:01:56 +0100

    Use 2 pieces of code

--- desktop.bundles/index/index.bemjson.js
+++ desktop.bundles/index/index.bemjson.js
@@ -8,11 +8,18 @@
         { elem: 'meta', attrs: { name: 'description', content: '' }}
     ],
     content:[
+        'Code blocks highlighted with Highlight.js',
         {
             block: 'highlight',
             mods: { theme: 'far' },
             lang: 'html',
             content: '<div class="my-block">'
+        },
+        {
+            block: 'highlight',
+            mods: { theme: 'solarized-dark' },
+            lang: 'js',
+            content: 'var $el = $(".el")'
         },
         { elem: 'js', url: '_index.js' }
     ]
