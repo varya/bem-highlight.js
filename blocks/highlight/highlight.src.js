27e808cff9f24d42e04c3e80cf6bba645b5b887b
Varya Stepanova <mail@varya.me>
2014-06-20 00:04:10 +0400

    Use old pack

--- blocks/highlight/highlight.src.js
+++ blocks/highlight/highlight.src.js
@@ -1,11 +1,6 @@
 modules.define('highlight__core', function(provide) {
 
-/*borschik:include:../../libs/highlight.js/src/highlight.js*/;
-/*borschik:include:../../libs/highlight.js/src/languages/css.js*/;
-/*borschik:include:../../libs/highlight.js/src/languages/bash.js*/;
-/*borschik:include:../../libs/highlight.js/src/languages/javascript.js*/;
-/*borschik:include:../../libs/highlight.js/src/languages/json.js*/;
-/*borschik:include:../../libs/highlight.js/src/languages/xml.js*/;
+/*borschik:include:src/highlight.pack.js*/;
 
 provide(hljs);
 
