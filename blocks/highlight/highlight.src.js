a17414b82889101762b58dd60a15df761e06cfc5
Varya Stepanova <mail@varya.me>
2014-06-19 23:02:19 +0400

    Include languages

--- blocks/highlight/highlight.src.js
+++ blocks/highlight/highlight.src.js
@@ -1,6 +1,11 @@
 modules.define('highlight__core', function(provide) {
 
-/*borschik:include:src/highlight.pack.js*/;
+/*borschik:include:../../libs/highlight.js/src/highlight.js*/;
+/*borschik:include:../../libs/highlight.js/src/languages/css.js*/;
+/*borschik:include:../../libs/highlight.js/src/languages/bash.js*/;
+/*borschik:include:../../libs/highlight.js/src/languages/javascript.js*/;
+/*borschik:include:../../libs/highlight.js/src/languages/json.js*/;
+/*borschik:include:../../libs/highlight.js/src/languages/xml.js*/;
 
 provide(hljs);
 
